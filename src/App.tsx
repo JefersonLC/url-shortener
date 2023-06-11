9b16941dc16a777d7769a780a99973fe63629e4b
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-10 19:16:07 -0500

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import { Suspense, lazy } from 'react';
 import { Route, Switch } from 'wouter';
+import Layout from './Layout';
 const Home = lazy(() => import('./pages/Home'));
 const Redirect = lazy(() => import('./pages/Redirect'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
-    <>
-      <nav className='bg-teal-400 px-5 flex gap-4'>
-        <h1>URL Shortener</h1>
-      </nav>
-      <main>
-        <Suspense fallback={<div>Cargando...</div>}>
-          <Switch>
-            <Route path='/' component={Home} />
-            <Route path='/redirect/:id' component={Redirect} />
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </main>
-    </>
+    <Layout>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/redirect/:id' component={Redirect} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Layout>
   );
 }
 
