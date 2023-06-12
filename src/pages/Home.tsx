4b6f99a708b32e092cca8a578d3f67602450151a
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-11 23:04:03 -0500

    modified home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { object, string } from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import SUI from 'short-unique-id';
+import { object, string } from 'yup';
 
 interface FormValues {
   url: string;
@@ -25,21 +25,42 @@ export default function Home() {
   };
 
   return (
-    <Formik
-      onSubmit={handleSubmit}
-      initialValues={initialValues}
-      validationSchema={createUrl}
-    >
-      <Form>
-        <Field
-          name='url'
-          placeholder='URL - Ex: http://your.site.com'
-          autoComplete='off'
-        />
-        <ErrorMessage name='url' />
-
-        <button type='submit'>OK</button>
-      </Form>
-    </Formik>
+    <main className='min-h-screen bg-slate-800 flex flex-col px-4'>
+      <section className='flex-grow basis-0 flex flex-col justify-end'>
+        <p className='text-lg text-slate-50'>Enter your URL:</p>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          validationSchema={createUrl}
+        >
+          <Form>
+            <div className='mb-3 mt-2'>
+              <Field
+                name='url'
+                placeholder='Ex: http://your.site.com'
+                autoComplete='off'
+                className='w-full p-3 rounded-md'
+              />
+              <ErrorMessage
+                name='url'
+                component='span'
+                className='block mt-1 text-sm text-rose-600'
+              />
+            </div>
+            <div className='flex justify-center'>
+              <button
+                type='submit'
+                className='bg-emerald-400 px-3 py-1 mx-auto rounded-lg'
+              >
+                Generate link
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </section>
+      <section className='flex-grow basis-0'>
+        <p>Aquì va el nuevo link</p>
+      </section>
+    </main>
   );
 }
