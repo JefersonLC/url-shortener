12647ddf87cb0da18a6d4e58eb46d5544e69276d
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-11 23:04:41 -0500

    modified Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -13,14 +13,16 @@ export default function Layout({ children }: LayoutProps) {
           </span>
         </h1>
       </nav>
-      <main className='min-h-screen bg-slate-800'>{children}</main>
+      {children}
       <footer className='fixed bottom-0 left-0 right-0 py-2'>
-        <p className='text-center text-slate-50'>
+        <p className='text-center text-sm sm:text-base text-slate-50'>
           Powered by{' '}
           <a
             href='https://github.com/JefersonLC'
             target='_blank'
-            className='relative hover:text-amber-500 [&>span]:w-0 [&>span]:hover:w-full '
+            className={`relative hover:text-amber-500 pr-6 pl-4 mx-1 [&>span]:w-0 [&>span]:hover:w-full 
+            after:absolute after:content-["<"] after:left-0 after:text-slate-500
+            before:absolute before:content-["/>"] before:right-0 before:text-slate-500`}
           >
             @JefersonLC
             <span className='absolute bottom-0 left-0 h-[1px] bg-amber-500 transition-all' />
