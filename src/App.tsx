53fddeda91cfd2ddd25e233ce665d0f71701577d
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-15 21:15:39 -0500

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { Suspense, lazy } from 'react';
 import { Route, Switch } from 'wouter';
 import Layout from './Layout';
+import UrlContextProvider from './context/UrlContext';
 const Home = lazy(() => import('./pages/Home'));
 const Redirect = lazy(() => import('./pages/Redirect'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
-    <Layout>
-      <Switch>
-        <Suspense fallback={<div className='pt-40'>Skeleton...</div>}>
-          <Route path='/' component={Home} />
-        </Suspense>
-        <Suspense fallback={<div className='pt-40'>Cargando...</div>}>
-          <Route path='/:id' component={Redirect} />
-          <Route component={NotFound} />
-        </Suspense>
-      </Switch>
-    </Layout>
+    <UrlContextProvider>
+      <Layout>
+        <Switch>
+          <Suspense fallback={<div className='pt-40'>Skeleton...</div>}>
+            <Route path='/' component={Home} />
+          </Suspense>
+          <Suspense fallback={<div className='pt-40'>Cargando...</div>}>
+            <Route path='/:id' component={Redirect} />
+            <Route component={NotFound} />
+          </Suspense>
+        </Switch>
+      </Layout>
+    </UrlContextProvider>
   );
 }
 
