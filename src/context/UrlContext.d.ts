520cd2061ed2114bddca08a3cdb914cfb269e981
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-15 22:11:44 -0500

    UrlContext.d.ts

--- src/context/UrlContext.d.ts
+++ src/context/UrlContext.d.ts
@@ -4,8 +4,22 @@ export interface NewUrl {
   id: string;
   url: string;
 }
+
+export interface UrlData {
+  id: string;
+  url: string;
+  created_at: string;
+}
+
 export interface ContextValues {
   newUrl: (values: NewUrl) => Promise<PostgrestSingleResponse<null>>;
+  handleLoad: (state: boolean) => void;
+  getUrl: <Interface>(id: string) => Promise<{
+    status: number;
+    data: Interface;
+  }>;
+  url: string;
+  isLoading: boolean;
 }
 
 export interface ProviderProps {
