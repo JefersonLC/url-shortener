47cb1802d79d8c4c292df3382832b9c0f76bf70f
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-15 22:11:51 -0500

    UrlContext

--- src/context/UrlContext.tsx
+++ src/context/UrlContext.tsx
@@ -1,19 +1,35 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { supabase } from '../supabase';
 import { ContextValues, NewUrl, ProviderProps } from './UrlContext.d';
 
 export const UrlContext = createContext<ContextValues>(null!);
 
 export default function UrlContextProvider(props: ProviderProps) {
+  const [url, setUrl] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function getUrl<Interface>(id: string) {
+    const { data, status } = await supabase
+      .from('short-links')
+      .select()
+      .eq('id', id)
+      .maybeSingle();
+    return { status, data: data as Interface };
+  }
+
   async function newUrl(values: NewUrl) {
     const response = await supabase.from('short-links').insert(values);
     return response;
   }
 
+  function handleLoad(state: boolean) {
+    setIsLoading(state);
+  }
+
   return (
-    <UrlContext.Provider value={{ newUrl }}>
+    <UrlContext.Provider value={{ newUrl, handleLoad, getUrl, url, isLoading }}>
       {props.children}
     </UrlContext.Provider>
   );
