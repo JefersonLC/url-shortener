115c7d560e05f1153e958086c33ff9c88731f66d
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-15 22:11:18 -0500

    updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import UrlForm from '../components/UrlForm';
+import { useUrl } from '../hooks/useUrl';
 import SadFace from '../icons/SadFace';
 
 export default function Home() {
+  const { url } = useUrl();
+
   return (
     <main
       className={`min-h-screen bg-slate-800 flex flex-col px-4 sm:px-24
@@ -13,12 +16,18 @@ export default function Home() {
       </section>
       <section className='flex-grow basis-6'>
         <div className='p-4 mt-10 border-dashed rounded-xl border-4 border-slate-400'>
-          <p className='text-center text-slate-400 text-lg 2xl:text-2xl mb-4'>
-            If you try to generate a url?
-          </p>
-          <span className='[&>svg]:fill-slate-400 flex justify-center'>
-            <SadFace />
-          </span>
+          {url ? (
+            'xd'
+          ) : (
+            <>
+              <p className='text-center text-slate-400 text-lg 2xl:text-2xl mb-4'>
+                If you try to generate a url?
+              </p>
+              <span className='[&>svg]:fill-slate-400 flex justify-center'>
+                <SadFace />
+              </span>
+            </>
+          )}
         </div>
       </section>
     </main>
