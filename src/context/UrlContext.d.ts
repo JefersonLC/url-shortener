2edb90ef4c1bbf938b352cc205d0b2c137efade9
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-15 21:16:28 -0500

    UrlContext.d.ts

--- src/context/UrlContext.d.ts
+++ src/context/UrlContext.d.ts
@@ -1,6 +1,11 @@
+import { PostgrestSingleResponse } from '@supabase/supabase-js';
+
 export interface NewUrl {
   id: string;
   url: string;
+}
+export interface ContextValues {
+  newUrl: (values: NewUrl) => Promise<PostgrestSingleResponse<null>>;
 }
 
 export interface ProviderProps {
