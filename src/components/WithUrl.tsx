27bb967181b0740e6172405b7c62ea9770884a14
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-16 22:39:12 -0500

    WithUrl component

---
+++ src/components/WithUrl.tsx
@@ -0,0 +1,13 @@
+export default function WithUrl({ children }: { children: string }) {
+  return (
+    <>
+      <p className='text-center text-slate-400 text-lg 2xl:text-2xl mb-4'>
+        Your new url has been generated
+      </p>
+      <p className='text-center text-lg 2xl:text-2xl'>{children}</p>
+      <span className='[&>svg]:fill-slate-400 flex justify-center'>
+        {/* TODO: Add smiley face icon*/}
+      </span>
+    </>
+  );
+}
