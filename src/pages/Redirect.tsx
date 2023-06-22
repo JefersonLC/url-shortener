77893f998e9a1dce6886eee49c52d2b05d961256
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-21 21:22:32 -0500

    updated Redirect page

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,10 +1,31 @@
-import {useRoute} from 'wouter'
+import { DotSpinner } from '@uiball/loaders'
+import { useEffect } from 'react'
+import { useRoute } from 'wouter'
+import { navigate } from 'wouter/use-location'
+import { UrlData } from '../context/UrlContext.d'
+import { useUrl } from '../hooks/useUrl'
 
 export default function About() {
+  const [, params] = useRoute('/:id')
+  const { getUrl } = useUrl()
 
-  const  [, params]= useRoute('/redirect/:id')
+  useEffect(() => {
+    if (!params) return navigate('/')
+    ;(async () => {
+      const { data } = await getUrl<UrlData>(params.id)
+
+      if (!data) return navigate('/')
+
+      return (location.href = data.url)
+    })()
+  }, [])
 
   return (
-    <div>{params?.id}</div>
+    <main className='min-h-screen bg-slate-800 flex items-center justify-center'>
+      <div className='text-center flex flex-col items-center justify-center'>
+        <p className='text-lg text-slate-50'>Redirecting...</p>
+        <DotSpinner size={60} color='#F8FAFC' />
+      </div>
+    </main>
   )
 }
