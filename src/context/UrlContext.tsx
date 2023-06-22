12f2849fa312d8d5196249db83ec0f6b0ef48d46
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-21 21:21:06 -0500

    updated getUrl

--- src/context/UrlContext.tsx
+++ src/context/UrlContext.tsx
@@ -1,35 +1,35 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-import { createContext, useState } from 'react';
-import { supabase } from '../supabase';
-import { ContextValues, NewUrl, ProviderProps } from './UrlContext.d';
+import { createContext, useState } from 'react'
+import { supabase } from '../supabase'
+import { ContextValues, NewUrl, ProviderProps } from './UrlContext.d'
 
-export const UrlContext = createContext<ContextValues>(null!);
+export const UrlContext = createContext<ContextValues>(null!)
 
 export default function UrlContextProvider(props: ProviderProps) {
-  const [url, setUrl] = useState<string>('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   async function getUrl<Interface>(id: string) {
     const { data, status } = await supabase
       .from('short-links')
       .select()
       .eq('id', id)
-      .maybeSingle();
-    return { status, data: data as Interface };
+      .maybeSingle()
+    return { status, data: data as Interface | null }
   }
 
   async function newUrl(values: NewUrl) {
-    const response = await supabase.from('short-links').insert(values);
-    return response;
+    const response = await supabase.from('short-links').insert(values)
+    return response
   }
 
   function handleLoad(state: boolean) {
-    setIsLoading(state);
+    setIsLoading(state)
   }
 
   function handleUrl(newUrl: string) {
-    setUrl(newUrl);
+    setUrl(newUrl)
   }
 
   return (
@@ -38,5 +38,5 @@ export default function UrlContextProvider(props: ProviderProps) {
     >
       {props.children}
     </UrlContext.Provider>
-  );
+  )
 }
