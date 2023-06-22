8b8947f6c8f20fabfa579cf1fe3c23e903276d14
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-21 21:21:25 -0500

    updated getUrl data

--- src/context/UrlContext.d.ts
+++ src/context/UrlContext.d.ts
@@ -1,28 +1,28 @@
-import { PostgrestSingleResponse } from '@supabase/supabase-js';
+import { PostgrestSingleResponse } from '@supabase/supabase-js'
 
 export interface NewUrl {
-  id: string;
-  url: string;
+  id: string
+  url: string
 }
 
 export interface UrlData {
-  id: string;
-  url: string;
-  created_at: string;
+  id: string
+  url: string
+  created_at: string
 }
 
 export interface ContextValues {
-  newUrl: (values: NewUrl) => Promise<PostgrestSingleResponse<null>>;
-  handleLoad: (state: boolean) => void;
+  newUrl: (values: NewUrl) => Promise<PostgrestSingleResponse<null>>
+  handleLoad: (state: boolean) => void
   getUrl: <Interface>(id: string) => Promise<{
-    status: number;
-    data: Interface;
-  }>;
-  handleUrl: (newUrl: string) => void;
-  url: string;
-  isLoading: boolean;
+    status: number
+    data: Interface | null
+  }>
+  handleUrl: (newUrl: string) => void
+  url: string
+  isLoading: boolean
 }
 
 export interface ProviderProps {
-  children: ReactNode;
+  children: ReactNode
 }
