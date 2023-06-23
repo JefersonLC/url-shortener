c7f1ca7a19837e6a6cdfe577151e27fbc943c7c7
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-22 21:24:42 -0500

    updated NtFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,25 @@
+import { Link } from 'wouter'
+import useLocation from 'wouter/use-location'
+import SadFace from '../icons/SadFace'
+
 export default function NotFound() {
-  return <div>Not Found</div>
+  const [location] = useLocation()
+
+  return (
+    <main className='min-h-screen bg-slate-800 flex justify-center items-center'>
+      <div className='flex flex-col gap-2 justify-center items-center [&>svg]:fill-slate-50'>
+        <p className='text-center text-xl text-slate-50'>
+          Path <span className='text-emerald-400 font-mono'>"{location}"</span>{' '}
+          doesn't exist
+        </p>
+        <SadFace />
+        <Link
+          to='/'
+          className='py-2 px-4 bg-emerald-400 mt-4 cursor-pointer hover:text-emerald-400 hover:bg-slate-900'
+        >
+          Back to home
+        </Link>
+      </div>
+    </main>
+  )
 }
